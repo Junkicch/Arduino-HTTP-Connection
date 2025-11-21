197ed51238b0c3c7e6cd6e830411faff282dc197
Junkicch <76850261+Junkicch@users.noreply.github.com>
2025-11-21 15:27:33 -0300

    Update network_config.h

--- network_config.h
+++ network_config.h
@@ -1,7 +1,8 @@
 #pragma once
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(10, 100, 0, 50);        // livre na faixa do roteador
-IPAddress gateway(10, 100, 0, 1);    // gateway do roteador
+IPAddress ip(, , , );        // ip livre na faixa do roteador, para ip fixo
+IPAddress gateway(, , , );    // gateway do roteador
 IPAddress subnet(255, 255, 0, 0);
-IPAddress serverIp(10, 100, 0, 145); // IP que seu notebook recebeu
+IPAddress serverIp(, , , ); // IP do seu servidor
+
 #define SERVER_PORT 8080            // porta que vamos abrir no PC
